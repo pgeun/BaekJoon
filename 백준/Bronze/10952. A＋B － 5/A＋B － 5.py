e95c474fd6c2a442b9945f6e1988c657ed662e6e
pgeun <145344441+pgeun@users.noreply.github.com>
2024-07-10 16:42:20 +0900

    [Bronze V] Title: A+B - 5, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -0,0 +1,10 @@
+import sys
+
+A, B = map(int, sys.stdin.readline().split())
+print(A+B)
+
+while True :
+    A, B = map(int, sys.stdin.readline().split())
+    if A == 0 and B == 0 :
+        break
+    print(A+B)
