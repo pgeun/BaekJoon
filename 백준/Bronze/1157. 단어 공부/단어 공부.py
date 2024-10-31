b9efce861c612ca7bcb305f9c599457f4c34e63d
pgeun <145344441+pgeun@users.noreply.github.com>
2024-10-31 12:57:24 +0900

    [Bronze I] Title: 단어 공부, Time: 176 ms, Memory: 33076 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,21 @@
+A = input().upper()
+
+alphabet = {'A':0, 'B':0, 'C':0, 'D':0, 'E':0, 'F':0, 'G':0, 'H':0, 'I':0, 'J':0, 'K':0, 'L':0, 'M':0, 'N':0, 'O':0, 'P':0, 'Q':0, 'R':0, 'S':0, 'T':0, 'U':0, 'V':0, 'W':0, 'X':0, 'Y':0, 'Z':0}
+find_index = {0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:'G', 7:'H', 8:'I', 9:'J', 10:'K', 11:'L', 12:'M', 13:'N', 14:'O', 15:'P', 16:'Q', 17:'R', 18:'S', 19:'T', 20:'U', 21:'V', 22:'W', 23:'X', 24:'Y', 25:'Z'}
+
+for i in A:
+    alphabet[i] += 1
+
+all_value = list(alphabet.values())
+max_value = max(all_value)
+count = 0
+
+for n in alphabet.values():
+    if n == max_value:
+        count += 1
+        
+if count > 1:
+    print('?')
+else:
+    found_index = all_value.index(max_value)
+    print(find_index[found_index])
