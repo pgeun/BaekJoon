95964395e9c69652966d1e4dff4bedb7465e4f30
pgeun <145344441+pgeun@users.noreply.github.com>
2025-04-02 18:25:44 +0900

    [Silver V] Title: 분수찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -7,24 +7,8 @@ while result + count <= N:
     count += 1
 
 if count % 2 == 0:
-    start = [count-1, 0]
-    for _ in range(N-result):
-        start[0] -= 1
-        start[1] -= 1
+    start = [(count-1)-(N-result), -(N-result)]
 else :
-    start = [0, -(count-1)]
-    for _ in range(N-result):
-        start[0] += 1
-        start[1] += 1
-        
-if count == 1:
-    print('1/1')
-else:
-    for _ in range(2) :
-        if start[_] < 0 :
-            start[_] -= 1
-        else:
-            start[_] += 1
-    print(abs(start[1]), end='')
-    print('/', end='')
-    print(abs(start[0]))
+    start = [(N-result), -(count-1)+(N-result)]
+
+print(abs(start[1])+1, '/', abs(start[0])+1, sep='')
