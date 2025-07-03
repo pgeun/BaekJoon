d3dc32c308aa12ddb71b1bfd875d22b3f40489c2
pgeun <145344441+pgeun@users.noreply.github.com>
2025-07-03 11:54:01 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
@@ -4,10 +4,10 @@ while (True):
     if sum(side) == 0:
         break
     if side[2]<side[0]+side[1]:
-        side = sorted(set(side))
-        if len(side) == 3:
+        unique = len(set(side))
+        if unique == 3:
             print('Scalene')
-        elif len(side) == 2:
+        elif unique == 2:
             print('Isosceles')
         else:
             print('Equilateral')
